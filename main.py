656cab94fd2cbe846f94c5a0a1718fff4afe6487
Boris Dayma <boris.dayma@gmail.com>
2021-07-09 23:47:59 -0500

    fix: log every checkpoint

--- main.py
+++ main.py
@@ -503,7 +503,7 @@ if __name__ == "__main__":
             "image_logger": {
                 "target": "main.ImageLogger",
                 "params": {
-                    "batch_frequency": 750,
+                    "batch_frequency": 50,
                     "max_images": 4,
                     "clamp": True
                 }
